970d8356a55db217f8a05ae2fff508660827a6f8
pankajdudwa <pankajberwal882@gmail.com>
2026-03-28 23:23:16 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, set, update, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR KEY",
-  authDomain: "YOUR DOMAIN",
-  databaseURL: "YOUR DATABASE URL",
-  projectId: "YOUR ID",
-  storageBucket: "YOUR BUCKET",
-  messagingSenderId: "YOUR SENDER",
-  appId: "YOUR APP ID"
+  apiKey: "AIzaSyDsAwKJy63zW5ZQ4d7WLEK4Od3261AlpGo",
+  authDomain: "ipl-match-streaming.firebaseapp.com",
+  databaseURL: "https://ipl-match-streaming-default-rtdb.firebaseio.com",
+  projectId: "ipl-match-streaming",
+  storageBucket: "ipl-match-streaming.firebasestorage.app",
+  messagingSenderId: "354233195229",
+  appId: "1:354233195229:web:8dc281f9461da74c10d934",
+  measurementId: "G-JC6BJ7EZRJ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export { database, ref, set, update, onValue };
+const analytics = getAnalytics(app);
